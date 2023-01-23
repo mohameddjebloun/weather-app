4dacf8faa5a16ba6f1b86a5350e167549755daf6
mohameddjebloun <djeblounm@gmail.com>
2023-01-23 10:12:34 +0100

    Create the Temperature component

---
+++ src/components/Weather/Temperature.js
@@ -0,0 +1,12 @@
+const Temperature = (props) => {
+  return (
+    <p className="text-center">
+      {!props.unit
+        ? `${parseFloat((props.data.main.temp - 273.15).toFixed(2))} °C`
+        : `${parseFloat(
+            (((props.data.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)
+          )} °F`}
+    </p>
+  );
+};
+export default Temperature;
