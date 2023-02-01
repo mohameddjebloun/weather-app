5a8cceda589740276e7b6a8bf3a1a0daee2f0ed7
mohameddjebloun <djeblounm@gmail.com>
2023-02-01 17:52:37 +0100

    Modify the App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import "./App.css";
-
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import Weather from "./components/Weather/Weather";
 function App() {
+  const [location, setLocation] = useState("Algiers");
+
   return (
-    <div className="hero min-h-screen bg-base-200 text-white">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
-    </div>
+    <>
+      <SearchBar setLocation={setLocation} /> <Weather location={location} />
+    </>
   );
 }
 
